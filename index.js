641aa924841e1009656de21faa573d35b86a3adf
💢chantatha💥 <chatatha.p@entroinca.co.th>
2019-08-24 00:35:43 +0700

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+/*!
+ * read-format-detail
+ * Copyright(c) 2019 Chantatha Polsamak <chantatha.p@gmail.com>
+ * MIT Licensed
+ */
+
+'use strict'
+
+const { version } = require('./package.json')
+const Utils = require('./utils')
+
+const argv = require('./node_modules/minimist')(process.argv.slice(2), {
+  alias: {
+    v: 'version',
+    d: 'diff'
+  }
+})
+
+// show version
+if (argv.v || argv.version || argv._[0] === 'version') {
+  process.stdout.write(version)
+} else if (argv.d || argv.diff || argv._[0] === 'diff') {
+  const iPath = argv.d
+  const util = new Utils()
+  util.getFile(iPath)
+} else {
+  process.stdout.write('invalid use lib.')
+}
---
+++ utils.js
@@ -0,0 +1,76 @@
+/*!
+ * read-format-detail
+ * Copyright(c) 2019 Chantatha Polsamak <chantatha.p@gmail.com>
+ * MIT Licensed
+ */
+
+'use strict'
+
+const fs = require('fs')
+
+function Utils () {}
+
+Utils.prototype.getFile = (rPath) => {
+  let cFile = 0
+  let cLine = []
+  let errLine = []
+
+  try {
+    // console.log(rPath)
+    fs.readdir(rPath, function (err, files) {
+      // handling error
+      if (err) {
+        return process.stderr.write('Unable to scan directory: ' + err + '\n')
+      }
+      // listing all files using forEach
+      files.forEach(function (file) {
+        cFile++
+        const getNameFile = file.split('.')
+        if (getNameFile[getNameFile.length - 1] === 'detail') {
+          const logPath = rPath + '/' + file
+
+          fs.readFile(logPath, (err, data) => {
+            // handling error
+            if (err) {
+              return process.stderr.write('Unable to scan directory: ' + err + '\n')
+            }
+            process.stdout.write('No.' + cFile + ' Validating: ' + getNameFile[0] + '...\n')
+            process.stdout.write('--------------------\n')
+            const rawDatas = ('' + data).replace(/\d.+\|/gm, ',').split(/,{/gm)
+            for (const rawData of rawDatas) {
+              if (rawData) {
+                cLine++
+                try {
+                  var objData = JSON.parse('{' + rawData)
+                } catch (error) {
+                  errLine.push(cLine)
+                  process.stderr.write('Line: ' + cLine + ' status: ' + '--------> invalid <--------\n')
+                }
+                if (objData.Input.length !== 1 && objData.Input.length !== 1) {
+                  errLine.push(cLine)
+                  process.stderr.write('Line: ' + cLine + ' status: ' + '--------> invalid <--------\n')
+                }
+              }
+            }
+            // summary
+            process.stdout.write('====================\n')
+            if (errLine.length === 0) {
+              process.stdout.write('SUMMARY FILENAME:' + getNameFile[0])
+              process.stdout.write(' = ALL SUCCESS.\n')
+            } else {
+              process.stdout.write('SUMMARY FILENAME:' + getNameFile[0])
+              process.stdout.write(' = GOT ERROR.\n')
+              process.stdout.write('Line:' + errLine + '\n')
+            }
+          })
+        } else {
+          process.stdout.write(file + ' is not detail. \n')
+        }
+      })
+    })
+  } catch (error) {
+    console.error('error: ', error)
+  }
+}
+
+module.exports = Utils
